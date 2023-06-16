1ceda426d95ba2ea38e4f285b80a892fcccfe0fd
Ahmedben69 <123458760+Ahmedben69@users.noreply.github.com>
2023-06-16 15:09:40 +0200

    Update init-script.sh

--- prod/init-script.sh
+++ prod/init-script.sh
@@ -8,4 +8,8 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 #Creating container
-sudo docker run -d -p 8080:1234 --name ahmed  Ahmedben69/ahmedbrief14:latest
+sudo docker docker login -u bainos69 -p Supersayien69
+sudo docker build -t ahmed14 .
+sudo docker tag ahmed14:latest bainos69/ahmed14:latest
+sudo docker push bainos69/ahmed14:latest 
+sudo docker run -d -p 8080:1234 --name ahmed14 bainos69/
